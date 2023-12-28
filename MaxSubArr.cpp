5241a86e963812460cc7403a8d4adbf9b02c4e63
jackr276 <jack_m_robbins@me.com>
2023-12-28 14:03:05 -0500

    program fully functional, complete

--- MaxSubArr.cpp
+++ MaxSubArr.cpp
@@ -3,65 +3,87 @@
  * 12/28/2023
  * 
  * %%%%%%%%%%%%%%%%%% Problem Description %%%%%%%%%%%%%%%%%%%
- * Given two arrays of nonzero positive integers, arr1 and arr2, of equal size,
+ * Given an array of k nonzero positive integers,
  * find the length n of the largest possible subarray in
  * which all elements are increasing, taking the "i"th element
  * from arr1 and the "i"th element from array 2 at any given time
- * 
- * Example: arr1 = [2, 1, 4, 2], [4, 3, 2, 1]
- * i = 0: take 2 from arr1; result = [2]
- * i = 1: take 3 from arr2; result = [2, 3]
- * i = 2: take 4 from arr1; result = [2, 3, 4]
- * i = 3: no valid answers
- * 
- * Max subarray = [2, 3, 4]
 */
 
 
 //For printing
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <set>
+#include <time.h>
 
 using namespace std;
 
 
 //predeclare
-vector<vector<int>> findAllIncrSubArrays(int array[], int size);
+set<vector<int>> findAllIncrSubArrays(int array[], int size);
+vector<int> findMaxSubArray(int (&arr)[], int size);
 bool isIncreasing(vector<int> subArr);
 
+
+
 int main(){
-    int arr1[] = {2, 1, 4, 2};
-    int arr2[] = {4, 3, 2, 1};
-
-
-    int result[4];
-    
-    vector<vector<int>> arr1Subs = findAllIncrSubArrays(arr1, sizeof(arr1)/sizeof(arr1[0]));
-
-    for(auto &row: arr1Subs){
-        for(auto &column: row){
-            cout << column << " ";
-        }
-        cout << endl;
+    int arr[100];
+    //generate a random array
+    cout << "Initial Array:\n[";
+    srand(time(NULL));
+    for(int i = 0; i < 100; i++){
+        //cut the random numbers down some
+        arr[i] = rand()%1000;
+        cout << arr[i]<< " ";
     }
+    cout << "]" << endl;
+
+    vector<int> maxSubArray = findMaxSubArray(arr, 100);
+
+    cout << "Largest increasing subarray\n[";
+    
+    for (auto &elem : maxSubArray){
+        cout << elem << ",";
+    }
+
+    cout << "]" << endl;
 }
 
 
 
-bool findMaxSubArray(int arr1[], int arr2[], int (&result)[]){
+vector<int> findMaxSubArray(int (&arr)[], int size){
+    //only want unique entries
+    set<vector<int>> arr1Subs = findAllIncrSubArrays(arr, size);
+    //result
+    vector<int> vec;
 
+    cout << "All increasing subarrays generated:" << endl; 
+    int max = 0;
 
-    return true;
+    //Show the user all subarrays that are increasing
+    for(auto &row: arr1Subs){
+        cout << "[";
+        if (row.size() > max){
+            max = row.size();
+            vec = row;
+        }
+        for(auto &column: row){
+            cout << column << ",";
+        }
+        cout << "]" << endl;
+    }
 
+    return vec;
 }
 
 
 /**
  * Iteratively find all nonempty subarrays
 */
-vector<vector<int>> findAllIncrSubArrays(int array[], int size){
+set<vector<int>> findAllIncrSubArrays(int array[], int size){
     //Stores all of our subarrays
-    vector<vector<int>> subarrays;
+    set<vector<int>> subarrays;
 
     //starting point
     for(int i = 0; i < size; i++){
@@ -76,7 +98,7 @@ vector<vector<int>> findAllIncrSubArrays(int array[], int size){
 
             //we only want tempvec if it is increasing
             if (isIncreasing(tempVec)){
-                subarrays.push_back(tempVec);
+                subarrays.insert(tempVec);
             }
 
         }
